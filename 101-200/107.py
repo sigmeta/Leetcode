5224e88334966bba56b4546015cb0a122362597f
Sigmeta <sigmeta@pku.edu.cn>
2018-05-14 09:27:19 +0800

    Binary Tree Level Order Traversal II

---
+++ 101-200/107.py
@@ -0,0 +1,24 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def levelOrderBottom(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[List[int]]
+        """
+        if not root:return []
+        res=[]
+        valsnow=[];nodesnow=[];nodesbefore=[root]
+        while nodesbefore!=[]:
+            res.append([node.val for node in nodesbefore])
+            for node in nodesbefore:
+                if node.left: nodesnow.append(node.left)
+                if node.right: nodesnow.append(node.right)
+            nodesbefore=nodesnow
+            nodesnow=[]
+        return res[::-1]
