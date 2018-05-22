d6a0ce7726bda59476361d85f8acf76566fdae7f
Sigmeta <sigmeta@pku.edu.cn>
2018-05-22 13:06:02 +0800

    Minimum Depth of Binary Tree. 93.86%

---
+++ 101-200/111.py
@@ -0,0 +1,18 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def minDepth(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        if not root:return 0
+        elif not root.left and not root.right:return 1
+        elif not root.left:return self.minDepth(root.right)+1
+        elif not root.right:return self.minDepth(root.left)+1
+        else:return min(self.minDepth(root.left),self.minDepth(root.right))+1
