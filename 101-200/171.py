1b9cff769584f561fe3fc19ac8c0446ec8e88f97
Sigmeta <sigmeta@pku.edu.cn>
2018-12-03 15:31:00 +0800

    excel-sheet-column-number

---
+++ 101-200/171.py
@@ -0,0 +1,10 @@
+class Solution:
+    def titleToNumber(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        res=0
+        for c in s:
+            res=res*26+ord(c)-ord('A')+1
+        return res
