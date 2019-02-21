712066b47f6f1441b6ebb0e241382409a5237414
Sigmeta <sigmeta@pku.edu.cn>
2019-02-21 20:06:30 +0800

    house-robber

---
+++ 101-200/198.py
@@ -0,0 +1,10 @@
+class Solution:
+    def rob(self, nums: 'List[int]') -> 'int':
+        if not nums:
+            return 0
+        for i in range(2,len(nums)):
+            if i==2:
+                nums[i]+=nums[0]
+            else:
+                nums[i]+=max(nums[i-2],nums[i-3])
+        return max(nums[-2:])
