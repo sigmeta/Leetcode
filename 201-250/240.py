6b31bc1fa2d641608b32111471bbb8ee20c36b3a
Sigmeta <sigmeta@pku.edu.cn>
2019-06-23 14:38:10 +0800

    Create 240.py

---
+++ 201-250/240.py
@@ -0,0 +1,11 @@
+class Solution:
+    def searchMatrix(self, matrix, target):
+        """
+        :type matrix: List[List[int]]
+        :type target: int
+        :rtype: bool
+        """
+        for line in matrix:
+            if target in line:
+                return True
+        return False
